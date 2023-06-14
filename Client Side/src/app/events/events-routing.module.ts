e21d38198012b88205e7fbcd1902b8ac4b092b3a
Ahmed Abdelnaser <112420984+ahmedabdelnaser70@users.noreply.github.com>
2023-06-14 18:39:29 +0300

    header-router

--- Client Side/src/app/events/events-routing.module.ts
+++ Client Side/src/app/events/events-routing.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { EventsComponent } from './events.component';
+import { EventDetailsComponent } from './Components/event-details/event-details.component';
 
-
+const routes: Routes = [
+  { path: '', component: EventsComponent },
+  { path: 'details', component: EventDetailsComponent },
+];
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [CommonModule, RouterModule.forChild(routes)],
+  exports: [RouterModule],
 })
-export class EventsRoutingModule { }
+export class EventsRoutingModule {}
