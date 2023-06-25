60ddb6a5e748ac40a526a52de0594424d770855d
AlaaElshahat <82738655+AlaaElshahat@users.noreply.github.com>
2023-06-26 02:10:32 +0300

    .

--- Dashboard/src/app/Speakers/speakers.component.ts
+++ Dashboard/src/app/Speakers/speakers.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+// import { Component } from '@angular/core';
 
-@Component({
-  selector: 'app-speakers',
-  templateUrl: './speakers.component.html',
-  styleUrls: ['./speakers.component.scss']
-})
+// @Component({
+//   selector: 'app-speakers',
+//   templateUrl: './speakers.component.html',
+//   styleUrls: ['./speakers.component.scss']
+// })
 export class SpeakersComponent {
 
 }
