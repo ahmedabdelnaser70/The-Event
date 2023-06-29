a5e70b004b597009524f2128a6b27633f17486d1
zeinab elazab <zeinabelazzab875@gmail.com>
2023-06-29 18:41:16 +0300

    addresetpass

--- Client Side/src/app/account/login/login.component.ts
+++ Client Side/src/app/account/login/login.component.ts
@@ -6,6 +6,7 @@ import { AuthService } from '../Services/auth.service';
 import { UserStoreService } from '../Services/user-store.service';
 import ValidateForm from '../../helpers/validationform';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { ResetPasswordServiceService } from '../Services/reset-password-service.service';
 
 @Component({
   selector: 'app-login',
@@ -18,12 +19,17 @@ export class LoginComponent implements OnInit{
   type: string = 'password';
   isText: boolean = false;
   eyeIcon: string = 'fa-eye-slash';
+  public resetPasswordEmail!:string;
+  public isValidEmail !:Boolean;
+
+
   constructor(
     private fb: FormBuilder,
     private auth: AuthService,
     private router: Router,
     private toast: NgToastService,
     private userStore: UserStoreService,
+    private resetpass:ResetPasswordServiceService
   
   ) {}
 
@@ -61,6 +67,33 @@ export class LoginComponent implements OnInit{
       });
     } else {
       ValidateForm.validateAllFormFields(this.loginForm);
+    }
+  }
+  checkValidEmail(event:string){
+    const value = event;
+    const pattern =  /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/;
+    this.isValidEmail=pattern.test(value);
+   return this.isValidEmail;
+  }
+
+  confirmToSend(){
+    if(this.checkValidEmail(this.resetPasswordEmail)){
+      console.log(this.resetPasswordEmail);
+    
+      this.resetpass.sendResetPasswordLink(this.resetPasswordEmail).
+      subscribe({
+        next:(res=>{
+          this.toast.success({detail:"SUCCESS", summary:"Reset Password success", duration: 5000});
+          this.resetPasswordEmail="";
+          const buttonRef = document.getElementById("closebtn");
+          buttonRef?.click();
+
+        }),
+        error:(err)=>{
+          this.toast.error({detail:"ERROR", summary:"Something when wrong!", duration: 5000});
+          console.log(err);
+        }
+      })
     }
   }
 }
