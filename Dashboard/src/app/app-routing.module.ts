e5a253dff25ef3ab00b376517274fd94f831cd1c
zeinab elazab <zeinabelazzab875@gmail.com>
2023-06-30 04:15:45 +0300

    addpro

--- Dashboard/src/app/app-routing.module.ts
+++ Dashboard/src/app/app-routing.module.ts
@@ -4,12 +4,9 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './account/login/login.component';
 import { EventsComponent } from './Events/events.component';
 import { SpeakersComponent } from './Speakers/speakers.component';
-<<<<<<< HEAD
 import { UsersComponent } from './users/users/users.component';
-=======
 import { SponsersComponent } from './Sponsers/sponsers.component';
 import { NotFoundComponent } from './NotFound/not-found.component';
->>>>>>> 1197081383acc139af870b9b317fa458eacc435f
 
 
 const routes: Routes = [
@@ -17,7 +14,6 @@ const routes: Routes = [
   { path: 'events', component: EventsComponent },
   // { path: 'login', component: LoginComponent },
   { path: 'speakers', component: SpeakersComponent },
-<<<<<<< HEAD
  // { path: 'sponsers', component:  SponserComponent},
   {
     path: 'account',
@@ -25,20 +21,15 @@ const routes: Routes = [
       import('./account/account.module').then((m) => m.AccountModule),
   },
    { path: 'users', component:UsersComponent},
-=======
   { path: 'sponsers', component: SponsersComponent },
->>>>>>> 1197081383acc139af870b9b317fa458eacc435f
   // { path: 'tickets', component:  },
   // { path: 'hotels', component:  },
   // { path: 'gallary', component:  },
   // { path: 'eventvenue', component:  },
-<<<<<<< HEAD
   { path: '', redirectTo: 'account/login', pathMatch: 'full' },
   // {path: "**", component: DNotFoundComponent},
-=======
 
   { path: '**', component: NotFoundComponent },
->>>>>>> 1197081383acc139af870b9b317fa458eacc435f
 ];
 
 @NgModule({
